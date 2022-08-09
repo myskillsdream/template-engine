2d93120a0625128e40bbc07f25b03668e4744b27
myskillsdream <myskillsdream@gmail.com>
2022-08-09 22:00:54 +0100

    render template called index

--- server.js
+++ server.js
@@ -6,12 +6,25 @@ const fccTesting = require('./freeCodeCamp/fcctesting.js');
 
 const app = express();
 
+const session = require('express-session');
+const passport = require('passport');
+
 app.set('view engine', 'pug')
 
 fccTesting(app); //For FCC testing purposes
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+  cookie: { secure: false }
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.route('/').get((req, res) => {
 
