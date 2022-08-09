2da678096e73c74313cef29465a5d05320882b20
myskillsdream <myskillsdream@gmail.com>
2022-08-09 21:15:53 +0100

    render template called index

--- server.js
+++ server.js
@@ -6,12 +6,16 @@ const fccTesting = require('./freeCodeCamp/fcctesting.js');
 
 const app = express();
 
+app.set('view engine', 'pug')
+
 fccTesting(app); //For FCC testing purposes
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.route('/').get((req, res) => {
+
+  res.render(views/pug/index)
 
 });
 
